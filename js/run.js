9df091950c97dec88572b84e2cc5b8a8e33fcdf6
iki1uc <entertrainer@web.de>
2026-09-14 09:27:19 +0200

    Update run.js

--- js/run.js
+++ js/run.js
@@ -1,1 +1,10 @@
+async function loadData() {
+  const [axioms, welcome, run, room] = await Promise.all([
+    fetch('data/axioms.json').then(r => r.json()),
+    fetch('data/welcome.json').then(r => r.json()),
+    fetch('data/run.json').then(r => r.json()),
+    fetch('data/room.json').then(r => r.json())
+  ]);
+  S.data = { axioms, welcome, run, room };
+}
 
